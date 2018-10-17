0a3d5d01c750ac52f397dd003d4a4aeb8d0584af
tshaffer54 <shaftr01@luther.edu>
2018-10-17 15:46:59 -0500

    edit nameserver

--- project3/nameserver.py
+++ project3/nameserver.py
@@ -77,41 +77,45 @@ def read_zone_file(filename: str) -> tuple:
     zone = dict()
     with open(filename) as zone_file:
         origin = zone_file.readline().split()[1].rstrip('.')
-        # ttl1 = zone_file.readline().split()[1]
-        # for line in zone_file:
-        #     line = line.split()
-        #     if len(line) == 4:
-        #         print(line)
-        #         dom = list(zone.keys())[-1]
-        #         domain = dom
-        #         ttl = line[0]
-        #         clas = line[1]
-        #         typ = line[2]
-        #         addr = line[3]
-        #         zone[domain].append([ttl, clas, typ, addr])
-        #         print(zone)
-        #     if len(line) == 3:
-        #         print(line)
-        #         dom = list(zone.keys())[-1]
-        #         domain = dom
-        #         clas = line[0]
-        #         typ = line[1]
-        #         addr = line[2]
-        #         ttl = ttl1
-        #         zone[domain].append([ttl, clas, typ, addr])
-        #         print(zone)
-        #     else:
-        #         print(line)
-        #         domain = line[0]
-        #         ttl = line[1]
-        #         clas = line[2]
-        #         typ = line[3]
-        #         addr = line[4]
-        #         zone.update({domain: [ttl, clas, typ, addr]})
-        #         print(zone)
+        ttl1 = zone_file.readline().split()[1]
+        for line in zone_file:
+            line = line.split()
+            if len(line) == 4:
+                if TTL_SEC.__contains__(line[0]):
+                    dom = list(zone.keys())[-1]
+                    domain = dom
+                    ttl = line[0]
+                    clas = line[1]
+                    typ = line[2]
+                    addr = line[3]
+                    zone[domain].append([ttl, clas, typ, addr])
+                else:
+                    domain = line[0]
+                    ttl = ttl1
+                    clas = line[1]
+                    typ = line[2]
+                    addr = line[3]
+                    if zone.__contains__(domain):
+                        zone[domain].append([ttl1, clas, typ, addr])
+                    else:
+                        zone.update({domain: [ttl, clas, typ, addr]})
+            elif len(line) == 3:
+                dom = list(zone.keys())[-1]
+                domain = dom
+                clas = line[0]
+                typ = line[1]
+                addr = line[2]
+                ttl = ttl1
+                zone[domain].append([ttl, clas, typ, addr])
+            else:
+                domain = line[0]
+                ttl = line[1]
+                clas = line[2]
+                typ = line[3]
+                addr = line[4]
+                zone.update({domain: [ttl, clas, typ, addr]})
 
         # raise NotImplementedError
-    
     return origin, zone
 
 
